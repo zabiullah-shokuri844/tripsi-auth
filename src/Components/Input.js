a404a5e28404b851f9ba1fdf704b93417df6d0b3
zabiullah-shokuri4 <zabitawana4@gmail.com>
2024-02-05 22:11:58 +0330

    Include on component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,3 +1,5 @@
+
+
 const Input = (props) => {
     return (
 
@@ -10,7 +12,8 @@ const Input = (props) => {
                  justify-content-start py-sm-2 py-1  lightsuccess`}>
                     <div className="d-flex align-items-center pb-1">{props.icon}</div>
 
-                    <input type={props.type} className="px-2 w-100 d-flex align-items-center bg-third-light  m-0 h-auto custome-input-border" />
+                    <input type={props.type} value={props.value} onChange={props.onChange} className="px-2 w-100 d-flex align-items-center bg-third-light  m-0 h-auto custome-input-border" />
+
                 </div>
             </label>
             <div className="text-danger validation-container w-100 d-flex justify-content-start px-3 pt-1 overflow-auto font-small"></div>
