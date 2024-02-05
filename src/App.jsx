72e60567bab4e6d1c4a55d39e2cd119ffd44b2f3
zabiullah-shokuri4 <zabitawana4@gmail.com>
2024-02-05 09:54:56 +0330

    set router on app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Login from './Login/login';
+import Signup from './SignUp/Signup';
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+
+      </BrowserRouter>
+
+    </>
+  );
+}
+export default App;
